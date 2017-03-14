68c215caecb7fe5d0e61e6908ca5c4f6ec470bbc
noblowski <ja.noblowski@gmail.com>
2017-03-14 17:51:54 +0100

    zmiany

---
+++ lab3/MinimalTimeDifference.cpp
@@ -0,0 +1,32 @@
+//
+// Created by pilanorb on 14.03.17.
+//
+
+#include <vector>
+#include <sstream>
+#include <regex>
+#include <cmath>
+#include <MinimalTimeDifference.h>
+
+using::std::regex;
+using::std::smatch;
+
+regex pattern {R"(\d{1,2}):(\d{2})"};
+
+unsigned int ToMinutes(std::string time_HH_MM);
+
+ToInt(string value)
+    int int_value;
+    stringstring ss;
+    ss << value;
+    ss >> int_value;
+    return int_value;
+
+class Solution {
+public:
+    unsigned int MinimalTimeDifference(std::vector<std::string> times){
+    if (vector < 12:00){
+            return (24:00 - vector);
+        }
+    }
+};
---
+++ lab3/MinimalTimeDifference.h
@@ -0,0 +1,13 @@
+//
+// Created by pilanorb on 14.03.17.
+//
+
+#ifndef JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H_H
+#define JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H_H
+
+#endif //JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H_H
+
+#include <cmath>
+#include <vector>
+#include <string>
+

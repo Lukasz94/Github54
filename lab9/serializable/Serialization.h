4fb768610a5246d5e08a01e61012ddd2607d1c34
noblowski <ja.noblowski@gmail.com>
2017-05-09 18:06:52 +0200

    zmiany9

--- lab9/serializable/Serialization.h
+++ lab9/serializable/Serialization.h
@@ -2,47 +2,45 @@
 // Created by pilanorb on 09.05.17.
 //
 
-#include <vector>
-#include <iostream>
-
 #ifndef JIMP_EXERCISES_SERIELIZABLE_H
 #define JIMP_EXERCISES_SERIELIZABLE_H
+#include <vector>
+#include <iostream>
+#include <string>
+#include <virtual>
 
-class academia {
-    class Serializable{
-        void Serializer(){};
+namespace academia {
+    class Serializable;
+
+    class Serializer {
+
+    public:
+
+        virtual:void IntegerField(const std::string &field_name, int value) = 0;
+
+        virtual:void DoubleField(const std::string &field_name, double value) = 0;
+
+        virtual:void StringField(const std::string &field_name, const std::string &value) = 0;
+
+        virtual:void BooleanField(const std::string &field_name, bool value) = 0;
+
+        virtual:void SerializableField(const std::string &field_name, const academia::Serializable &value) = 0;
+
+        virtual:void ArrayField(const std::string &field_name,
+                        const std::vector <std::reference_wrapper<const academia::Serializable>> &value) = 0;
+
+        virtual:void Header(const std::string &object_name) = 0;
+
+        virtual:void Footer(const std::string &object_name) = 0;
+
+    protected:
+        std::ostream &out;
     };
-};
 
-class Serializer {
+    class Serializable {
 
-public:
-
-    void IntegerField(const std::string &field_name, int value) = 0;
-
-    void DoubleField(const std::string &field_name, double value) = 0;
-
-    void StringField(const std::string &field_name, const std::string &value) = 0;
-
-    void BooleanField(const std::string &field_name, bool value) = 0;
-
-    void SerializableField(const std::string &field_name, const academia::Serializable &value) = 0;
-
-    void ArrayField(const std::string &field_name,
-                    const vector<reference_wrapper<const academia::Serializable>> &value) = 0;
-
-    void Header(const std::string &object_name) = 0;
-
-    void Footer(const std::string &object_name) = 0;
-
-protected:
-    std::ostream &out;
-};
-
-class Serializable {
-
-public:
-    void Serializer (Serializer * serializer);
-};
-
+    public:
+        void Serializer(Serializer *serializer);
+    };
+}
 #endif //JIMP_EXERCISES_SERIELIZABLE_H
